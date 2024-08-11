a23973383160ae9fb768b2b118caec531593b12a
Artem <artemvovk7777777@gmail.com>
2024-08-11 17:30:35 +0200

    delete rubbish from ReferenceService

--- src/main/java/com/server/catalog/services/ReferenceService.java
+++ src/main/java/com/server/catalog/services/ReferenceService.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.server.catalog.DTO.ReferenceDTO;
-import com.server.catalog.DTO.CompanyDTO.OfferDTO;
-import com.server.catalog.models.Photo;
 import com.server.catalog.models.Reference;
 import com.server.catalog.repositorys.referenceRepository;
 
@@ -44,19 +42,6 @@ public class ReferenceService {
 				.map(ref -> convertToDTO(ref))
 				.collect(Collectors.toList());
 		
-		
-//		List<Photo> photoList = photoDTOList.stream()
-//				.map(photoDTO -> convertToEntity(new Photo(), photoDTO))
-//				.collect(Collectors.toList());
-//		
-//		List<Photo> savedPhotosList = photoRepository.saveAll(photoList);				
-//		
-//		return savedPhotosList
-//				.stream()
-//				.map(photo -> convertToDTO(photo))
-//				.collect(Collectors.toList());
-		
-		
 	}
 	
 
@@ -86,55 +71,6 @@ public class ReferenceService {
 		ref.setSortOrder(refDTO.getSortOrder());
 		
 		return ref;
-	}
-
-//	public List<Reference> saveCategoryDTOList(List<CategoryDtoTEMP> categoryDtoList) {
-//		
-//		List<Reference> referenceList = categoryDtoList.stream()
-//				.map(category -> convertCategoryToEntity(category))
-//				.collect(Collectors.toList());
-//		
-//		List<Reference> savedReferenceList = referenceRepository.saveAll(referenceList);
-//		
-//		return savedReferenceList;
-//	}
-//
-//	private Reference convertCategoryToEntity(CategoryDtoTEMP category) {
-//
-//		Reference reference = new Reference();
-//		
-//		reference.setKey(category.getCatKey());
-//		reference.setValue(category.getCatValue());
-//		reference.setSortOrder(category.getOrder());
-//		
-//		reference.setScope("company_charact_category");
-//		
-//		return reference;
-//	}
-//
-//	public List<Reference> saveCharDTOList(List<СharacteristicDtoTEMP> charDtoList) {
-//
-//		List<Reference> referenceList = charDtoList.stream()
-//				.map(charact -> convertCharToEntity(charact))
-//				.collect(Collectors.toList());
-//		
-//		List<Reference> savedReferenceList = referenceRepository.saveAll(referenceList);
-//		
-//		return savedReferenceList;
-//	}
-//
-//	private Reference convertCharToEntity(СharacteristicDtoTEMP charact) {
-//
-//		Reference reference = new Reference();
-//
-//		reference.setKey(charact.getCharKey());
-//		reference.setValue(charact.getCharValue());
-//		reference.setIcon(charact.getIcon());
-//		
-//		reference.setScope("company_cgaracteristic");
-//
-//		return reference;
-//	}
-	
+	}	
 	
 }
